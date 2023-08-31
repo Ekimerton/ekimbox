8263299203b5b0968938e5c17bbd0ee142cd9699
Ekimerton <ekim0252@gmail.com>
2023-08-31 19:40:03 -0400

    Fix

--- client/src/components/host/BackgroundMusic.js
+++ client/src/components/host/BackgroundMusic.js
@@ -4,42 +4,50 @@ import bgMusic from "./sounds/bgmusic4.wav";
 import bgMusic2 from "./sounds/bgmusic5.wav";
 
 function BackgroundMusic({ stage, notificationApi }) {
-  const [play1, { stop: stop1, sound: sound1 }] = useSound(bgMusic, {
+  const [isMusic1Loaded, setMusic1Loaded] = useState(false);
+  const [isMusic2Loaded, setMusic2Loaded] = useState(false);
+
+  const [play1, { stop: stop1 }] = useSound(bgMusic, {
     loop: true,
     volume: 0.2,
+    onload: () => setMusic1Loaded(true),
   });
 
-  const [play2, { stop: stop2, sound: sound2 }] = useSound(bgMusic2, {
+  const [play2, { stop: stop2 }] = useSound(bgMusic2, {
     loop: true,
     playbackRate: 0.8,
     volume: 0.2,
+    onload: () => setMusic2Loaded(true),
   });
-
-  console.log(sound1);
 
   useEffect(() => {
     // Check loading status for music
-    if (!sound1 || !sound2) {
+    if (!isMusic1Loaded || !isMusic2Loaded) {
       notificationApi.loading({
-        content: "Trying to connect to server...",
+        content: "Loading music...",
         duration: 0,
         style: { width: "100%", alignItems: "center" },
       });
+    } else {
+      notificationApi.destroy(); // Destroy loading notification once both tracks are loaded
     }
-  }, [sound1, sound2]);
+  }, [isMusic1Loaded, isMusic2Loaded]);
 
   useEffect(() => {
-    if (!stage || stage === "register") {
-      play1();
-    } else {
-      play2();
+    if (isMusic1Loaded && isMusic2Loaded) {
+      // Ensure music is loaded before playing
+      if (!stage || stage === "register") {
+        play1();
+      } else {
+        play2();
+      }
     }
 
     return () => {
       stop1();
       stop2();
     };
-  }, [stage]);
+  }, [stage, isMusic1Loaded, isMusic2Loaded]); // Add the loaded states to the dependency array
 
   return null; // This component doesn't render anything visually
 }
