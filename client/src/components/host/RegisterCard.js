45d959465de2c723a4550ecb464f47a4583e88b3
Ekimerton <ekim0252@gmail.com>
2023-08-31 16:34:35 -0400

    New grunt sounds 0_0

--- client/src/components/host/RegisterCard.js
+++ client/src/components/host/RegisterCard.js
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from "react";
 import PlayerView from "../PlayerView";
-import grunts from "../host/sounds/grunts.wav";
+// import grunts from "../host/sounds/grunts.wav";
+import grunts from "../../sounds/grunts2.wav";
 import useSound from "use-sound";
 
 function RegisterCard({ gameState }) {
   const [prevPlayerNames, setPrevPlayerNames] = useState("");
 
+  /*
   const [playGrunt] = useSound(grunts, {
     sprite: {
       1: [80, 300],
@@ -15,9 +17,25 @@ function RegisterCard({ gameState }) {
     },
     volume: 0.8,
   });
+  */
+
+  const [playGrunt] = useSound(grunts, {
+    sprite: {
+      1: [430, 300],
+      2: [1050, 550],
+      3: [2253, 250],
+      4: [3800, 800],
+      5: [5730, 370],
+      6: [7000, 500],
+      7: [7680, 430],
+      8: [8500, 500],
+      9: [9180, 300],
+      10: [10920, 450],
+    },
+  });
 
   function getRandomSprite() {
-    const spriteKeys = ["1", "2", "3", "4"];
+    const spriteKeys = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
     const randomIndex = Math.floor(Math.random() * spriteKeys.length);
     return spriteKeys[randomIndex];
   }
